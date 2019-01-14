fadc174d1bfc2c72bf4e9dbe6a3373dab42543c1
mtj0928 <mtj0928@users.noreply.github.com>
2019-01-14 11:57:33 +0900

    Covert HashMap to ConcurrentHashMap

--- node/src/main/java/jp/kusumotolab/kgenprog/coordinator/LoadBalancer.java
+++ node/src/main/java/jp/kusumotolab/kgenprog/coordinator/LoadBalancer.java
@@ -2,16 +2,16 @@ package jp.kusumotolab.kgenprog.coordinator;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 import jp.kusumotolab.kgenprog.grpc.Worker;
 
 public class LoadBalancer {
 
   final List<Worker> allWorkerList = new ArrayList<>();
-  final Map<Worker, Integer> taskMap = new HashMap<>();
+  final Map<Worker, Integer> taskMap = new ConcurrentHashMap<>();
 
   public void addWorker(final Worker worker) {
     allWorkerList.add(worker);
@@ -29,15 +29,12 @@ public class LoadBalancer {
         .get();
 
     final Worker worker = minEntry.getKey();
-    final Integer task = minEntry.getValue();
-
-    taskMap.put(worker, task + 1);
+    taskMap.compute(worker, (k, v) -> v + 1);
     return worker;
   }
 
   public void finish(final Worker worker) {
-    final Integer task = taskMap.get(worker);
-    taskMap.put(worker, task - 1);
+    taskMap.compute(worker, (k, v) -> v - 1);
   }
 
   public List<Worker> getWorkerList() {
