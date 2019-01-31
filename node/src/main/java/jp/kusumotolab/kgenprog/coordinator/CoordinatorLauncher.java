3e1f81c043b44e2b65b61d9f3e827fa256021a7f
Ryo Arima <a3636tako@gmail.com>
2019-01-31 13:50:49 +0900

    log destinationを引数で指定できるように変更

--- node/src/main/java/jp/kusumotolab/kgenprog/coordinator/CoordinatorLauncher.java
+++ node/src/main/java/jp/kusumotolab/kgenprog/coordinator/CoordinatorLauncher.java
@@ -1,6 +1,13 @@
 package jp.kusumotolab.kgenprog.coordinator;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 import jp.kusumotolab.kgenprog.grpc.ClusterConfiguration;
 
 public class CoordinatorLauncher {
@@ -18,7 +25,42 @@ public class CoordinatorLauncher {
   }
 
   public void launch(final ClusterConfiguration config) throws IOException, InterruptedException {
+    setLogConfig(config);
     final Coordinator coordinator = new Coordinator(config);
     coordinator.start();
+  }
+
+  private void setLogConfig(final ClusterConfiguration config) {
+    final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+    try {
+      final JoranConfigurator configurator = new JoranConfigurator();
+      configurator.setContext(context);
+      //context.reset();
+      final InputStream inputStream = createLogConfigXML(config.getLogDestination());
+      configurator.doConfigure(inputStream);
+    } catch (final JoranException e) {
+      e.printStackTrace();
+    }
+    StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+
+  }
+
+  private InputStream createLogConfigXML(final String destination) {
+    final String configXML = new StringBuilder() //
+        .append("<configuration>")
+        .append(
+            "  <appender name=\"stash\" class=\"net.logstash.logback.appender.LogstashTcpSocketAppender\">")
+        .append("    <destination>")
+        .append(destination)
+        .append("    </destination>")
+        .append("    <encoder class=\"net.logstash.logback.encoder.LogstashEncoder\" />")
+        .append("  </appender>")
+        .append("  <logger name=\"jp.kusumotolab.kgenprog.coordinator.log.Events\" level=\"info\">")
+        .append("    <appender-ref ref=\"stash\" />")
+        .append("  </logger>")
+        .append("</configuration>")
+        .toString();
+    return new ByteArrayInputStream(configXML.getBytes());
   }
 }
